74b62b78474ad9ec8cffed6be777df89b8b9598c
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-03-04 20:52:41 +0530

    structure padding

--- structure size.c
+++ structure size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>  
 struct student  
 {  
-   char a;  
-   char b;  
-   int c;  
+   char a;  //1byte
+   char b;  //1 byte
+   int c;  //4 bytes
 };  
 int main()  
 {  
    struct student stud1; // variable declaration of the student type..  
    // Displaying the size of the structure student.  
-   printf("The size of the student structure is %d", sizeof(stud1));  
+   printf("The size of the student structure is %d", sizeof(stud1)); //structure padding is done here 
    return 0;  
 }  //output=8
